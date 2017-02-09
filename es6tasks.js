b5ca20bb54dd9c8ca2902ef531f576f38d22b6f7
leknaat <wishfire@gmail.com>
2017-02-09 17:45:40 +0000

    Everything done, will await corrections.

--- es6tasks.js
+++ es6tasks.js
@@ -34,6 +34,13 @@ console.log(stringInt);
 
 
 
+// modules
+import {name, age, birthYear} from 'person';
+console,log(name, age, birthYear);
+
+
+
+
 //destructuring
 var {user, clan} = {user: 'devon', clan: 'gangrel'};
 console.log(user, clan); // devon gangrel
@@ -54,3 +61,42 @@ function logSpread(num1, num2, num3){
 }
 var nums = [7, 8];
 console.log(logSpread(...nums, 9));
+
+
+
+
+//classes
+class Pemdas {
+	constructor(num1, num2, num3) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+	}
+	get answer() {
+		return this.calc();
+	}
+	calc(){
+		return ((this.num1 * this.num1 * this.num1) / this.num2)*(this.num3 - this.num2 + this.num1);
+	}
+}
+const numbers = new Pemdas(6, 8, 15);
+console.log(numbers.answer);
+
+
+
+
+//Object rest and spread properties
+// i get this, but couldn't produce anything fancy...
+let { a, b, ...d } = { a: 2, b: 4, c: 6, d: 8 };
+console.log(a - b * (d.c) / (d.d)); // -1
+
+
+let { e, f, ...h } = { e: "Kayla", f: "Kate", g: "Bonnie", h: "Kate" };
+let newObject = { e, f, ...h };
+console.log(newObject); /*{
+						   Object {
+  						   "e": "Kayla",
+  						   "f": "Kate",
+  					       "g": "Bonnie",
+  						   "h": "Kate"
+ 								      }	*/
